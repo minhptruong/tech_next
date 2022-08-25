a24a4db45b5caa3775dae600efb842c380082633
Minh Truong <minhptruong1@gmail.com>
2022-08-25 17:09:10 -0400

    edits

--- frontend/technext/src/PatentSearch.js
+++ frontend/technext/src/PatentSearch.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const SERVER_URL = "http://192.168.1.220:8080/patents";
+const SERVER_URL = "https://192.168.1.220:8080/patents";
 
 export const PatentsApi = axios.create({
   baseURL: SERVER_URL,

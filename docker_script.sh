55649eaf54874f2176414513153390134a9bfcd8
Minh Truong <minhptruong1@gmail.com>
2022-08-25 18:14:31 -0400

    edits:

--- docker_script.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 336522104125.dkr.ecr.us-east-1.amazonaws.com
-sudo docker build -t technext_image .
-sudo docker tag technext_image:latest 336522104125.dkr.ecr.us-east-1.amazonaws.com/technext:latest
-docker push 336522104125.dkr.ecr.us-east-1.amazonaws.com/technext:latest
